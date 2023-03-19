564dbd6af8be0522cb2f87a5a84f4448915d2aba
Ahmet Gökay Ürkmez <gokayurkmez_541@hotmail.com>
2023-03-19 23:54:28 +0300

    -

--- Lab3/202011057_lab3_Ahmet_Gokay_Urkmez.cpp
+++ Lab3/202011057_lab3_Ahmet_Gokay_Urkmez.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
 
 using namespace std;
-
 
 class SparseMatrix{
 private:
@@ -9,30 +11,144 @@ private:
  	int cols;
  	int Nterms; // number of nonzero elements
  	int capacity; // size of smArray
+ 	//float ZeroPercentage;
+ 	int **sparse;
  	
 public:
-	//vector<int> smArray;
-	
 	SparseMatrix(int rows, int cols, int Nterms): rows{rows}, cols{cols}, Nterms{Nterms}, capacity{30} {
-		generateRandomSparseMatrix(rows, cols, 1.0*(rows*cols-Nterms)/(rows*cols)*100 ); // Burada Kaldýk
+		formSparse();
+		generateRandomSparseMatrix(); // Burada Kaldýk
 	}  
 	
-	SparseMatrix generateRandomSparseMatrix(int nrows, int ncols, int ZeroPercentage){
+	void generateRandomSparseMatrix(){
+		srand(time(NULL));
+		float ZeroPer =  calculateZeroPer();
+		int random;
+		
+		Nterms = 0;
+		
+		for(int i=0; i<rows; ++i) {
+			for(int j=0; j<cols; ++j) {
+				random = rand() % 100;
+				
+				if(random*1.0 > ZeroPer) {
+					sparse[i][j] = rand() % 9 + 1; 
+					++Nterms;
+				}			
+				else
+					sparse[i][j] = 0;
+			}
+		}
+	}
 	
+	void transpose() {
+		int tempint;
+		
+		int **temp;
+		temp = new int*[cols];
+		
+		for(int i=0; i<cols; ++i) {
+			temp[i] = new int[rows];
+		}
+			
+		for(int i=0; i<rows; ++i) {
+			for(int j=0; j<cols; ++j) {
+					temp[j][i] = sparse[i][j];
+			}
+		}
+		
+		tempint = cols;
+		cols = rows;
+		rows = tempint;
+		
+		copy(temp);
+		delete [] temp;
+	}
+	
+	void formSparse() {
+		sparse = new int*[rows];
+		
+		for(int i=0; i<rows; ++i) {
+			sparse[i] = new int[cols];
+		}
+	}
+	
+	float calculateZeroPer() {
+		return 1.0*(rows*cols-Nterms)/(rows*cols)*100;
+	}
+	
+	void copy(int **temp) {
+		formSparse();
+		
+		for(int i=0; i<rows; ++i) {
+			for(int j=0; j<cols; ++j) {
+					sparse[i][j] = temp[i][j];
+			}
+		}
+	}
+	
+	void Sparse() {
+		int tempint;
+		int k = 0;
+		
+		int **temp;
+		temp = new int*[3];
+		
+		for(int i=0; i<3; ++i) {
+			temp[i] = new int[Nterms];
+		}
+			
+		for(int i=0; i<rows; ++i) {
+			for(int j=0; j<cols; ++j) {
+				if(sparse[i][j] != 0) {
+					temp[0][k] = i+1;
+					temp[1][k] = j+1;
+					temp[2][k] = sparse[i][j];
+					++k;
+				}
+			}
+		}
+		
+		rows = 3;
+		cols= Nterms;
+		
+		copy(temp);
+		delete [] temp;
+	}
+	
+	~SparseMatrix() {
+		delete [] sparse;
 	}
 	
 	friend ostream& operator<<(ostream& OS, SparseMatrix& SM);
-	
-	
 };
 
 ostream& operator<<(ostream& OS, SparseMatrix& SM) {
-	/*
-    os << t.type << " " << t.make << " " << t.model << " " << t.automanu << " " << "-" << " " << t.price << " " << t.available;
-    return os;
-	*/
+	for(int i=0; i<SM.rows; ++i) {
+			for(int j=0; j<SM.cols; ++j) {
+					cout << SM.sparse[i][j] << " ";
+			}
+			cout << endl;
+	}
+	
+	return OS;
 }
 
+
 int main() {
-	
+ 	int rows, cols, Nterms;
+ 	int **arr1 = new int*[5];
+ 	arr1[0] = new int[5];
+ 	cout <<"Enter number of rows, columns and nonzero elements: ";
+ 	cin >> rows >> cols >>  Nterms;
+ 	
+ 	SparseMatrix s1(rows, cols, Nterms);
+
+ 	cout << "Original Matrix:" << endl << s1 << endl;
+ 	
+ 	s1.transpose();
+ 	cout << "Transposed Matrix:" << endl << s1 << endl;
+ 	
+ 	s1.Sparse();
+ 	cout << "Sparse Matrix:" << endl << s1;
 }
